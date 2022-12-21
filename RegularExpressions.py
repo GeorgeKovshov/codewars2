0642325a97f3082d63822d10614ea8c3f4497e09
GeorgeKovshov <GeorgGreat@gmail.com>
2022-12-21 03:52:12 +0300

    worked on regular expressions

--- RegularExpressions.py
+++ RegularExpressions.py
@@ -24,11 +24,36 @@ Mr. T
 
 sentence = "Start a sentence and bring it to an end"
 
+urls = '''
+https://www.google.com
+http://coreyms.com
+https://youtube.come
+https://www.nasa.gov
+'''
+
 print('\tTab')
 print(r'\tTab') #raw string
 
-pattern = re.compile(r'abc')
+pattern = re.compile(r'Ha')
+pattern2 = re.compile(r'start', re.I) # or re.IGNORECASE returns if all caps or none or between
+pattern3 = re.compile(r'sentence')
+
+matchss = pattern.findall((text_to_search))
 matches = pattern.finditer(text_to_search)
+
+match_start = pattern2.match(sentence) # matches at the beginning of a string
+match_search = pattern3.search(sentence) # finds the FIRST match and returns it
+print(match_start)
+print(match_search)
+
+urlsss = re.compile(r'https?://(www\.)?(\w+)(\.\w+)')
+urlsssZ = urlsss.finditer(urls)
 
 for match in matches:
     print(match)
+
+for match in urlsssZ:
+    print(match.group(2, 3))
+
+subbed_urls = urlsss.sub(r'\2\3', urls)
+print(subbed_urls)
